5f2f67997aa6fb6fa22d1c98eaf8acc1d2ea2b07
Julio A. <madth3.dev@gmail.com>
2014-11-25 00:29:06 -0600

    Clase de persistencia para preguntas.

---
+++ Cuestionarios/src/main/java/mx/lania/mca/cuestionarios/entidades/Pregunta.java
@@ -0,0 +1,141 @@
+package mx.lania.mca.cuestionarios.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author PCEL
+ */
+@Entity
+@Table(name = "PREGUNTA")
+@NamedQueries({
+    @NamedQuery(name = "Pregunta.findAll", query = "SELECT p FROM Pregunta p")})
+public class Pregunta implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "NUM")
+    private int num;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1000)
+    @Column(name = "TEXTO")
+    private String texto;
+    @Column(name = "FECHA_CREACION")
+    @Temporal(TemporalType.DATE)
+    private Date fechaCreacion;
+    @Size(max = 10)
+    @Column(name = "TIPO_PREGUNTA")
+    private String tipoPregunta;
+    @JoinColumn(name = "ID_CUESTIONARIO", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Cuestionario cuestionario;
+
+    public Pregunta() {
+    }
+
+    public Pregunta(Integer id) {
+        this.id = id;
+    }
+
+    public Pregunta(Integer id, int num, String texto) {
+        this.id = id;
+        this.num = num;
+        this.texto = texto;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getTipoPregunta() {
+        return tipoPregunta;
+    }
+
+    public void setTipoPregunta(String tipoPregunta) {
+        this.tipoPregunta = tipoPregunta;
+    }
+
+    public Cuestionario getCuestionario() {
+        return cuestionario;
+    }
+
+    public void setCuestionario(Cuestionario cuestionario) {
+        this.cuestionario = cuestionario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pregunta)) {
+            return false;
+        }
+        Pregunta other = (Pregunta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.lania.mca.cuestionarios.entidades.Pregunta[ id=" + id + " ]";
+    }
+    
+}
